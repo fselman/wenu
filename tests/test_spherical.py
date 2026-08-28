7496ee4008672278536d1378c666c2b344cc7fb2
Fernando <fselman@eso.org>
2026-08-28 17:04:48 -0400

    Restore repository newline convention

--- tests/test_spherical.py
+++ tests/test_spherical.py
@@ -581,3 +581,4 @@ class TestSphericalPolygons:
             match=rf"{field_name} must contain one value per entity",
         ):
             SphericalPolygons(coordinate_spec=GENERIC_SPHERICAL_SPEC, **arguments)
+
