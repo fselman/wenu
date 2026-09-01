e2d6c7f3a85d433babf489a597feddf3704bbf52
Fernando <fselman@eso.org>
2026-09-01 17:54:58 -0400

    Update Milky Way contour interface

--- tests/test_wenu_chart_cli.py
+++ tests/test_wenu_chart_cli.py
@@ -65,12 +65,12 @@ def test_planisphere_accepts_one_comma_separated_planet_selection():
     )]
 
 
-def test_all_chart_families_accept_lowest_milky_way_contour():
+def test_all_chart_families_accept_milky_way_contour():
     for family in EXPECTED_COMMANDS - {"defaults"}:
         arguments = chart.parser().parse_args([
-            family, "--mw-lowest-contour", "OL2",
+            family, "--mw-contour", "OL2",
         ])
-        assert arguments.mw_lowest_contour == "ol2"
+        assert arguments.mw_contour == "ol2"
 
 
 def test_regional_orientation_is_named_or_a_literal_angle():
