89e7a5cde97dcf96567aea522ec1af1ccf1ff030
Fernando <fselman@eso.org>
2026-09-01 10:23:56 -0400

    Add Mercury catalog validation contracts

--- src/wenu/sky/solar_system_catalog.py
+++ src/wenu/sky/solar_system_catalog.py
@@ -1,7 +1,8 @@
 """Built-in moving-body catalog; external catalogs can extend this value."""
 
 from wenu.sky.solar_system_bodies import SolarSystemBodyCatalog
+from wenu.sky.mercury import MERCURY_BODY
 from wenu.sky.venus import VENUS_POINT
 
 
-SOLAR_SYSTEM_BODY_CATALOG = SolarSystemBodyCatalog((VENUS_POINT,))
+SOLAR_SYSTEM_BODY_CATALOG = SolarSystemBodyCatalog((VENUS_POINT, MERCURY_BODY))
